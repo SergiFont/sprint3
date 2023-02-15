52f55d32ab3bc539402a35f6d1379ad64322d230
SergiFont <sfontsmx2010@gmail.com>
2023-02-15 16:53:48 +0100

    nivel1 entrega 3.2 completado

---
+++ entrega3.2/Middleware.js
@@ -0,0 +1,30 @@
+class Middleware {
+    constructor() {}
+
+    squaring(num1, num2) {
+        const output1 = num1 * num1
+        const output2 = num2 * num2
+        console.log(`El resultado de ${num1} al cuadrado es: ${output1}, y el resultado de ${num2} al cuadrado es: ${output2}`)
+        return this.cubed(output1, output2)
+    }
+
+    cubed(num1, num2) {
+        const output1 = num1 * num1 * num1
+        const output2 = num2 * num2 * num2
+        console.log(`Ahora elevamos ${num1} al cubo, siendo el resultado: ${output1}, y lo mismo con ${num2}: ${output2}`)
+        return this.divide(output1, output2)
+    }
+
+    divide(num1, num2) {
+        const output1 = num1 / 2
+        const output2 = num2 / 2
+        console.log(`Dividiendo ${num1} entre dos, el resultado es: ${output1}, y con ${num2} el resultado es: ${output2}`)
+        return { output1, output2 }
+    }
+
+    execute(num1, num2) {
+        this.squaring(num1, num2)
+    }
+}
+
+module.exports = { Middleware }
--- entrega3.2/app.js
+++ entrega3.2/app.js
@@ -1,6 +1,17 @@
-/*Middleware
-Crea en un fitxer inicial una petita aplicació que sumi, resti i multipliqui rebent els paràmetres en un JSON.
-Crea en un fitxer extern una classe que emmagatzemi middlewares (funcions).
-Insereix a la invocació middlewares que facin el quadrat, el cub i la divisió entre 2 dels operands inicials en 
-cadascuna de les operacions. Invoca les execucions de la suma, la resta i la multiplicació, de manera que es vagin 
-mostrant per la consola les modificacions que es van fent als valors abans del resultat final. */
+const { getNumbers } = require('./getNumbers.js')
+const { addition, subtraction, multiply } = require('./operations.js')
+const { Middleware } = require('./Middleware.js')
+
+
+
+const middleware = new Middleware
+
+const app = (operation) => {
+    const { num1, num2 } = getNumbers()
+    const {output1, output2} = middleware.squaring(num1, num2) 
+    operation(output1, output2)
+}
+
+app(addition)
+app(subtraction)
+app(multiply)
